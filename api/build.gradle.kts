3052e362c313bc71ada30aacf30adfea391c3eae
1552980358 <1552980358@qq.com>
2022-07-05 02:56:53 +0800

    api/build.gradle.kts: Add Xposed Api to androidTest

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -30,4 +30,5 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+    androidTestCompileOnly("de.robv.android.xposed:api:82")
 }
