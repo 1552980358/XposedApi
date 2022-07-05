7a424c35549446ba0fceb0575dddda15fbc37f99
1552980358 <1552980358@qq.com>
2022-07-06 03:50:51 +0800

    ExampleInstrumentedTest.kt: Update examples

--- api/src/androidTest/java/projekt/cloud/piece/xposed/api/ExampleInstrumentedTest.kt
+++ api/src/androidTest/java/projekt/cloud/piece/xposed/api/ExampleInstrumentedTest.kt
@@ -3,9 +3,12 @@ package projekt.cloud.piece.xposed.api
 import android.app.Activity
 import android.os.Bundle
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import de.robv.android.xposed.callbacks.XC_LoadPackage
 import org.junit.Test
 import org.junit.runner.RunWith
-import projekt.cloud.piece.xposed.api.method.Method.Companion.method
+import projekt.cloud.piece.xposed.api.find.method.CallMethod.call
+import projekt.cloud.piece.xposed.api.find.method.HookMethod.hook
+import projekt.cloud.piece.xposed.api.find.method.Method.method
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -16,26 +19,48 @@ import projekt.cloud.piece.xposed.api.method.Method.Companion.method
 class ExampleInstrumentedTest {
     
     @Test
-    fun hookActivityOnCreate() {
-        XposedHook.hookMethod {
-            clazz = Activity::class.java
-            /**
-             * Same as `method("onCreate", Bundle::class.java)`
-             **/
-            method = "onCreate"
-            params = arrayOf(Bundle::class.java)
-            before {
-                // Before onCreate() called
-            }
-            after {
-                // After onCreate() called
-            }
-        }
+    fun callActivityFinish(activity: Activity) {
+        // A quick finish() call with xposed callMethod()
+        method("finish").call(activity)
+        // DSL-styled
+        method {
+            method = "finish"
+            call(activity)  // Ignore result
+        }// .call(activity) to obtain result
     }
     
     @Test
-    fun callActivityFinish(activity: Activity) {
-        method("finish").call(activity)
+    fun hookActivityOnCreate(classLoader: ClassLoader?, loadPackageParam: XC_LoadPackage.LoadPackageParam?) {
+        // DSL-styled
+        method {
+            // With Class accessible directly
+            clazz = Activity::class.java
+            // With string Class path
+            // Both ClassLoader and LoadPackageParam acceptable
+            clazz("android.app.Activity", classLoader)
+            clazz("android.app.Activity", loadPackageParam)
+            // Same as `method("onCreate", Bundle::class.java)`
+            method = "onCreate"
+            // Same as `params = arrayOf(Bundle::class.java)`
+            params(Bundle::class.java)
+            before {
+                // Before onCreate called
+            }
+            after {
+                // After onCreate called
+            }
+            hook()  // Ignore result
+        }.hook()    // Obtain result returned by findAndHookMethod()
+        
+        // Builder-like style
+        method("onCreate", Bundle::class.java)
+            .before {
+                // Before onCreate called
+            }
+            .after {
+                // After onCreate called
+            }
+            .hook()
     }
     
 }
