300ba3f3ba870c3ea406c74669136b8b2aaffee8
1552980358 <1552980358@qq.com>
2022-07-06 03:44:12 +0800

    BaseFindWrapper.kt: Update base wrapper class

---
+++ api/src/main/java/projekt/cloud/piece/xposed/api/find/BaseFindWrapper.kt
@@ -0,0 +1,43 @@
+package projekt.cloud.piece.xposed.api.find
+
+import de.robv.android.xposed.XC_MethodHook
+import de.robv.android.xposed.XposedHelpers
+import de.robv.android.xposed.callbacks.XC_LoadPackage
+
+abstract class BaseFindWrapper {
+    
+    internal companion object {
+        fun <T: BaseFindWrapper> T.invokeBlock(block: T.() -> Unit) = apply(block)
+    }
+    
+    var clazz: Class<*>? = null
+    fun clazz(className: String, classLoader: ClassLoader?) {
+        clazz = XposedHelpers.findClass(className, classLoader)
+    }
+    fun clazz(className: String, loadPackageParam: XC_LoadPackage.LoadPackageParam?)
+        = clazz(className, loadPackageParam?.classLoader)
+    
+    var params: Array<Class<*>>? = null
+    fun params(vararg params: Class<*>) {
+        if (params.isNotEmpty()) {
+            this.params = arrayOf(*params)
+        }
+    }
+    var paramsObj: Array<Any?>? = null
+    fun params(vararg paramsObj: Any?) {
+        if (paramsObj.isNotEmpty()) {
+            this.paramsObj = arrayOf(*paramsObj)
+        }
+    }
+    
+    fun typedParams(params: Array<Class<*>>, vararg paramsObj: Any?) {
+        if (params.size == paramsObj.size && params.isNotEmpty()) {
+            this.params = params
+            this.paramsObj = arrayOf(*paramsObj)
+        }
+    }
+    
+    var before: ((XC_MethodHook.MethodHookParam) -> Unit)? = null
+    var after: ((XC_MethodHook.MethodHookParam) -> Unit)? = null
+    
+}
