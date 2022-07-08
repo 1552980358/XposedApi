e642b5696b4d282feca08c99c3188b8f6ee9b2f4
1552980358 <1552980358@qq.com>
2022-07-09 03:02:37 +0800

    find/field/SetField: Update static field setting

--- api/src/main/java/projekt/cloud/piece/xposed/api/find/field/SetField.kt
+++ api/src/main/java/projekt/cloud/piece/xposed/api/find/field/SetField.kt
@@ -6,9 +6,21 @@ import de.robv.android.xposed.XposedHelpers.findFieldIfExists
 object SetField {
 
     operator fun <T> FieldWrapper<T>.set(instance: Any, value: T?) =
-        findField(clazz, name).set(instance, value)
+        setImpl(instance, value)
+    
+    fun <T> FieldWrapper<T>.set(value: T?) =
+        setImpl(value = value)
     
     fun <T> FieldWrapper<T>.setExists(instance: Any, value: T?) =
+        setExistsImpl(instance, value)
+    
+    fun <T> FieldWrapper<T>.setExists(value: T?) =
+        setExistsImpl(value = value)
+    
+    private fun <T> FieldWrapper<T>.setImpl(instance: Any? = null, value: T?) =
+        findField(clazz, name).set(instance, value)
+    
+    private fun <T> FieldWrapper<T>.setExistsImpl(instance: Any? = null, value: T?) =
         findFieldIfExists(clazz, name).set(instance, value)
 
 }
