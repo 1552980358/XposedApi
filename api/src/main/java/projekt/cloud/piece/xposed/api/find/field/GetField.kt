3104822cbd6db6374e6f278129889a638c3792ab
1552980358 <1552980358@qq.com>
2022-07-09 03:06:42 +0800

    find/field/GetField: Update static field getting

--- api/src/main/java/projekt/cloud/piece/xposed/api/find/field/GetField.kt
+++ api/src/main/java/projekt/cloud/piece/xposed/api/find/field/GetField.kt
@@ -4,13 +4,21 @@ import de.robv.android.xposed.XposedHelpers.findField
 import de.robv.android.xposed.XposedHelpers.findFieldIfExists
 
 object GetField {
-
+    
+    operator fun <T> FieldWrapper<T>.get(instance: Any) = getImpl(instance)
+    
+    fun <T> FieldWrapper<T>.get() = getImpl()
+    
+    fun <T> FieldWrapper<T>.getExists(instance: Any) = getExistsImpl(instance)
+    
+    fun <T> FieldWrapper<T>.getExists() = getExistsImpl()
+    
     @Suppress("UNCHECKED_CAST")
-    operator fun <T> FieldWrapper<T>.get(instance: Any) =
+    private fun <T> FieldWrapper<T>.getImpl(instance: Any? = null) =
         findField(clazz, name).get(instance) as T
     
     @Suppress("UNCHECKED_CAST")
-    fun <T> FieldWrapper<T>.getExists(instance: Any) =
+    private fun <T> FieldWrapper<T>.getExistsImpl(instance: Any? = null) =
         findFieldIfExists(clazz, name)?.get(instance) as? T
     
 }
