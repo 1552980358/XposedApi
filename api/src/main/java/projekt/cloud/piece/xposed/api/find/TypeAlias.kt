a5c241cdb7974d872e345cb675d295b5e2644c1e
1552980358 <1552980358@qq.com>
2022-07-24 10:48:04 +0800

    find/TypeAlias: Update typealias for find members

--- api/src/main/java/projekt/cloud/piece/xposed/api/find/TypeAlias.kt
+++ api/src/main/java/projekt/cloud/piece/xposed/api/find/TypeAlias.kt
@@ -1,8 +1,16 @@
 package projekt.cloud.piece.xposed.api.find
 
 import de.robv.android.xposed.XC_MethodHook
+import projekt.cloud.piece.xposed.api.base.InvokeBlock
+import projekt.cloud.piece.xposed.api.find.constructor.ConstructorWrapper
+import projekt.cloud.piece.xposed.api.find.field.FieldWrapper
+import projekt.cloud.piece.xposed.api.find.method.MethodWrapper
 
-typealias InvokeBlock<T> = T.() -> Unit
+typealias ConstructorBlock = InvokeBlock<ConstructorWrapper>
+
+typealias MethodBlock = InvokeBlock<MethodWrapper>
+
+typealias FieldBlock<T> = InvokeBlock<FieldWrapper<T>>
 
 typealias MethodHookBlock = FindMethodParamWrapper.(XC_MethodHook.MethodHookParam) -> Unit
 
