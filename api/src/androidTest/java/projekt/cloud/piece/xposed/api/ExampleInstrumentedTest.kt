2f061c75a784d5dc7a115b2714034859610b90df
1552980358 <1552980358@qq.com>
2022-07-25 12:46:27 +0800

    ExampleInstrumentedTest.kt: Update examples

--- api/src/androidTest/java/projekt/cloud/piece/xposed/api/ExampleInstrumentedTest.kt
+++ api/src/androidTest/java/projekt/cloud/piece/xposed/api/ExampleInstrumentedTest.kt
@@ -1,33 +1,26 @@
 package projekt.cloud.piece.xposed.api
 
-import android.app.Activity
-import android.app.AlertDialog
-import android.app.Dialog
-import android.content.Context
-import android.os.Bundle
-import android.view.Menu
-import android.view.MenuInflater
-import android.view.autofill.AutofillManager
-import androidx.annotation.StyleRes
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import de.robv.android.xposed.IXposedHookZygoteInit
+import de.robv.android.xposed.callbacks.XC_InitPackageResources
 import de.robv.android.xposed.callbacks.XC_LoadPackage
-import org.junit.Test
 import org.junit.runner.RunWith
 import projekt.cloud.piece.xposed.api.find.BaseClassWrapper.Companion.clazz
 import projekt.cloud.piece.xposed.api.find.BaseClassWrapper.Companion.static
 import projekt.cloud.piece.xposed.api.find.BaseFindMethodWrapper.Companion.after
 import projekt.cloud.piece.xposed.api.find.BaseFindMethodWrapper.Companion.before
 import projekt.cloud.piece.xposed.api.find.BaseFindMethodWrapper.Companion.params
+import projekt.cloud.piece.xposed.api.find.BaseFindMethodWrapper.Companion.replace
 import projekt.cloud.piece.xposed.api.find.constructor.Constructor.constructor
 import projekt.cloud.piece.xposed.api.find.constructor.HookConstructor.hook
 import projekt.cloud.piece.xposed.api.find.field.Field.field
 import projekt.cloud.piece.xposed.api.find.field.GetField.get
-import projekt.cloud.piece.xposed.api.find.field.GetField.getExists
 import projekt.cloud.piece.xposed.api.find.field.SetField.set
-import projekt.cloud.piece.xposed.api.find.field.SetField.setExists
 import projekt.cloud.piece.xposed.api.find.method.CallMethod.call
+import projekt.cloud.piece.xposed.api.find.method.CallMethod.callStatic
 import projekt.cloud.piece.xposed.api.find.method.HookMethod.hook
 import projekt.cloud.piece.xposed.api.find.method.Method.method
+import projekt.cloud.piece.xposed.api.find.method.ReplaceMethod.replace
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -36,130 +29,210 @@ import projekt.cloud.piece.xposed.api.find.method.Method.method
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
-    
-    @Test
-    fun callActivityFinish(activity: Activity) {
-        // A quick finish() call with xposed callMethod()
-        method("finish").call(activity)
-        // DSL-styled
-        method {
-            method = "finish"   // No param exist in the method signature
-            call(activity)  // Ignore result
-        }// .call(activity) to obtain result
-    }
 
-    @Test
-    fun callResolveDialogTheme(context: Context, @StyleRes style: Int) {
-        // DSL style
-        method {
-            method = "resolveDialogTheme"
-            // paramsObj = arrayOf(context, style): Same as below params()
-            params(context, style)          // Same as [callActivityFinish], not to call params() no arg required to put
-            clazz = AlertDialog::class.java
-            // import projekt.cloud.piece.xposed.api.find.BaseClassWrapper.Companion.static
-            static()    // Mark as static
-        }.call()        // Same as `callStatic()`
+    class ExampleClass() {
 
-        // Builder style
-        method("resolveDialogTheme", context, style)
-            .clazz(AlertDialog::class.java)     // Same as `.static(AlertDialog::class.java)`
-            .static()   // import projekt.cloud.piece.xposed.api.find.BaseClassWrapper.Companion.static
-            .call()
-    }
-    
-    @Test
-    fun hookActivityOnCreate(classLoader: ClassLoader?, loadPackageParam: XC_LoadPackage.LoadPackageParam?) {
-        // DSL style
-        method {
-            // With Class accessible directly
-            clazz = Activity::class.java
-            // With string Class path
-            // Both ClassLoader and LoadPackageParam acceptable
-            clazz("android.app.Activity", classLoader)
-            clazz("android.app.Activity", loadPackageParam)
-            // Same as `method("onCreate", Bundle::class.java)`
-            method = "onCreate"
-            // Same as `params = arrayOf(Bundle::class.java)`
-            params(Bundle::class.java)
-            before {
-                // Before onCreate called
-            }
-            after {
-                // After onCreate called
-            }
-            hook()  // Ignore result
-        }.hook()    // Obtain result returned by findAndHookMethod()
-        
-        // Builder-like style
-        method("onCreate", Bundle::class.java)
-            .clazz(Activity::class.java)
-            .before {
-                // Before onCreate called
-            }
-            .after {
-                // After onCreate called
-            }
-            .hook()
-    }
-
-    fun hookDialogConstructor(classLoader: ClassLoader?, loadPackageParam: XC_LoadPackage.LoadPackageParam?) {
-        // DSL style
-        constructor {
-            clazz = Dialog::class.java
-            params(Context::class.java)
-            before {
-                // Before onCreate called
-            }
-            after {
-                // After onCreate called
-            }
-            hook()  // Ignore result
+        constructor(value: Int): this() {
+            this.value = value
         }
 
-        // Builder-like style
-        constructor("android.app.Dialog", loadPackageParam, Context::class.java)
-            .before {
-                // Before onCreate called
-            }
-            .after {
-                // After onCreate called
-            }
-            .hook()
+        companion object {
+
+            @JvmStatic
+            private fun getValue(instance: ExampleClass) =
+                instance.value
+
+            @JvmStatic
+            private val STATIC_VALUE = 99
+
+        }
+
+        private var value = 1
+
+        fun setValue(value: Int) {
+            this.value = value
+        }
+
+        private fun sum(value: Int): Int {
+            // ...
+            return this.value + value
+        }
+
     }
-    
-    fun accessActivityMenuInflater(activity: Activity) {
-        // DSL style
-        var menuInflater: MenuInflater =
-            field<MenuInflater> {
-                clazz = Activity::class.java
-                name = "menuInflater"
-            }.get(activity)     // [] can by applied, where the same as `get(...)`
-        
-        // Builder-like style with `[]` applied
-        menuInflater =
-            field<MenuInflater>(Dialog::class.java, "menuInflater")[activity]
-        
-        // Safely nullable, prevent from crash
-        val menuInflaterNullable: MenuInflater? =
-            field<MenuInflater>(Dialog::class.java, "menuInflater")
-                .getExists(activity)
-    }
-    
-    fun setActivityAutofillManager(activity: Activity) {
-        field<AutofillManager> {
-            clazz = Activity::class.java
-            name = "mAutofillManager"
-        }.set(activity,
-            activity.getSystemService(AutofillManager::class.java)
-        )   // [] can by applied, where the same as `get(...)`
-    
-        // Builder-like style with `[]` applied
-        field<AutofillManager>(Activity::class.java, "mAutofillManager")[activity] =
-            activity.getSystemService(AutofillManager::class.java)
-    
-        // Safely nullable, prevent from crash
-        field<AutofillManager>(Activity::class.java, "mAutofillManager")
-            .setExists(activity, activity.getSystemService(AutofillManager::class.java))
+
+    class MyXposedEntry: XposedEntry() {
+
+        override fun onZygote(startupParam: IXposedHookZygoteInit.StartupParam) {
+            // super method is redundant, just remove it if you like
+            // super.onZygote(startupParam)
+        }
+
+        override fun onLoadPackage(loadPackageParam: XC_LoadPackage.LoadPackageParam) {
+            // super method is redundant, just remove it if you like
+            // super.onLoadPackage(loadPackageParam)
+            methodHookingExample(loadPackageParam)
+            constructorHookingExample(loadPackageParam)
+            fieldHookingExample(loadPackageParam)
+        }
+
+        private fun methodHookingExample(loadPackageParam: XC_LoadPackage.LoadPackageParam) {
+            // Member method
+            // DSL-like
+            method {
+                clazz = ExampleClass::class.java
+
+                method = "setValue"     // Same as `method("setValue", Int::class.java)`
+                params(Int::class.java)
+
+                // import projekt.cloud.piece.xposed.api.find.BaseFindMethodWrapper.Companion.before
+                before {
+                    // Before method is called
+                }
+
+                after {
+                    // After method is called
+                }
+
+                // Hook it
+                // import projekt.cloud.piece.xposed.api.find.method.HookMethod.hook
+                hook()
+            }
+            // Builder-like
+            method("setValue", Int::class.java)
+                // Set class with full class name
+                .clazz("projekt.cloud.piece.xposed.api.ExampleInstrumentedTest:ExampleClass", loadPackageParam)
+                .before {
+                    // Before method is called
+                }.after {
+                    // After method is called
+                }.hook()    // import projekt.cloud.piece.xposed.api.find.method.HookMethod.hook
+
+            // Static method
+            method {
+                clazz = ExampleClass::class.java
+
+                method = "getValue"
+                params(ExampleClass::class.java)
+
+                // import projekt.cloud.piece.xposed.api.find.BaseFindMethodWrapper.Companion.before
+                before {
+                    // Before method is called
+                }
+
+                after {
+                    // After method is called
+                }
+
+                static()      // Mark as static
+
+                // Hook it
+                // import projekt.cloud.piece.xposed.api.find.method.HookMethod.hook
+                hook()
+            }
+            // All similar as above DSL style, but can have directly call next method
+            method("getValue", ExampleClass::class.java)
+                .before {  }
+                .after {  }
+                .static()
+                .hook()
+
+            // Replacement
+            method("getValue", ExampleClass::class.java) {
+                // import projekt.cloud.piece.xposed.api.find.BaseFindMethodWrapper.Companion.replace
+                replace {   // Replacement
+                    // Replace origin result
+                    setResult(0)
+                }
+                // import projekt.cloud.piece.xposed.api.find.method.ReplaceMethod.replace
+                replace()   // Replace method
+            }
+
+            // Requirement for calling method
+            method {
+                clazz = ExampleClass::class.java
+                method("setValue", Int::class.java)
+                after {
+                    // Call member method
+                    val sum = method("sum", 10)
+                        .clazz(ExampleClass::class.java)
+                        .call(it.thisObject)        // thisObject should be instance of ExampleClass inside `after` block
+                    // Call static method
+                    val value = method {
+                        clazz = ExampleClass::class.java
+                        method("getValue", it.thisObject)
+                    }.callStatic()      // Integration of .static() and .call()
+                }
+            }
+
+        }
+
+        private fun constructorHookingExample(loadPackageParam: XC_LoadPackage.LoadPackageParam) {
+            // DSL-like
+            constructor {
+                clazz = ExampleClass::class.java
+                params = arrayOf(Int::class.java)       // Array allowed, same as params()
+
+                // import projekt.cloud.piece.xposed.api.find.BaseFindMethodWrapper.Companion.before
+                before {
+                    // Before method is called
+                }
+
+                // import projekt.cloud.piece.xposed.api.find.BaseFindMethodWrapper.Companion.after
+                after {
+                    // After method is called
+                }
+
+                // Hook it
+                // import projekt.cloud.piece.xposed.api.find.constructor.HookConstructor.hook
+                hook()
+            }
+
+            // Builder-like         // If no params
+            constructor(ExampleClass::class.java)
+                .before {
+                    // Before method is called
+                }.after {
+                    // After method is called
+                }.hook()    // import projekt.cloud.piece.xposed.api.find.constructor.HookConstructor.hook
+        }
+
+        private fun fieldHookingExample(loadPackageParam: XC_LoadPackage.LoadPackageParam) {
+            constructor(ExampleClass::class.java)
+                .before {
+                    // Before method is called
+                }.after {
+                    // After method is called
+
+                    // Get `value`
+                    // If it is an static field, it will be the same procedure
+                    var value = field<Int> {
+                        clazz = ExampleClass::class.java
+                        name = "value"
+                    }.get(it.thisObject)
+
+                    // Directly get with operator get
+                    value = field<Int>("projekt.cloud.piece.xposed.api.ExampleInstrumentedTest:ExampleClass", loadPackageParam, "value")[it.thisObject]
+
+                    // Set `value`
+                    // If it is an member field
+                    value = 2
+                    field<Int> {
+                        clazz = ExampleClass::class.java
+                        name = "value"
+                    }[it.thisObject] = value
+
+                    // If it is an static field, apply with lambda
+                    field<Int>(ExampleClass::class.java, "STATIC_VALUE").set(value)
+                    
+                }.hook()    // import projekt.cloud.piece.xposed.api.find.constructor.HookConstructor.hook
+            
+        }
+
+        override fun onLoadResources(initPackageResourcesParam: XC_InitPackageResources.InitPackageResourcesParam) {
+            // super method is redundant, just remove it if you like
+            // super.onLoadResources(initPackageResourcesParam)
+        }
+
     }
     
 }
