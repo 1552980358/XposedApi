a122712b21909d24a057b2064666e62fd304e464
1552980358 <1552980358@qq.com>
2022-07-25 12:56:58 +0800

    XposedEntry.kt: Update fields

--- api/src/main/java/projekt/cloud/piece/xposed/api/XposedEntry.kt
+++ api/src/main/java/projekt/cloud/piece/xposed/api/XposedEntry.kt
@@ -1,5 +1,6 @@
 package projekt.cloud.piece.xposed.api
 
+import android.content.res.XModuleResources
 import de.robv.android.xposed.IXposedHookInitPackageResources
 import de.robv.android.xposed.IXposedHookLoadPackage
 import de.robv.android.xposed.IXposedHookZygoteInit
@@ -17,17 +18,40 @@ open class XposedEntry: IXposedHookZygoteInit,
     
     protected open fun onLoadResources(initPackageResourcesParam: InitPackageResourcesParam)
         = Unit
+
+    private var _packageName: String? = null
+    val packageName: String
+        get() = _packageName!!
+
+    var initPackageResourcesParam: InitPackageResourcesParam? = null
+
+    private var _moduleResources: XModuleResources? = null
+    protected val moduleResources: XModuleResources
+        get() = _moduleResources!!
+
+    private var _modulePath: String? = null
+    private val modulePath: String
+        get() = _modulePath!!
     
     override fun initZygote(startupParam: StartupParam) {
+        _modulePath = startupParam.modulePath
         onZygote(startupParam)
     }
     
     override fun handleLoadPackage(loadPackageParam: LoadPackageParam) {
+        _packageName = loadPackageParam.packageName
         onLoadPackage(loadPackageParam)
     }
     
     override fun handleInitPackageResources(initPackageResourcesParam: InitPackageResourcesParam) {
+        this.initPackageResourcesParam = initPackageResourcesParam
+        _moduleResources = XModuleResources.createInstance(modulePath, initPackageResourcesParam.res)
         onLoadResources(initPackageResourcesParam)
+
+        // Recycle
+        _modulePath = null
+        _packageName = null
+        this.initPackageResourcesParam = null
     }
     
 }
